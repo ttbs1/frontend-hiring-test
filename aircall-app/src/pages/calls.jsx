24e5513a35b4ac4deee50b54e2ea48d23ae78e06
Thiago Túlio Brandão Silva <47676524+ttbs1@users.noreply.github.com>
2021-11-17 06:28:08 -0300

    doing some tests

--- aircall-app/src/pages/calls.jsx
+++ aircall-app/src/pages/calls.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { getCalls } from "../util/api-con";
+import "./calls.css"
 //import { useNavigate } from "react-router-dom";
 
 export function Calls() {
@@ -8,17 +9,25 @@ export function Calls() {
 
     useEffect(() => {
         var user = JSON.parse(sessionStorage.user_data);
-        getCalls(user.access_token).then(response => setCalls({
+        getCalls(user).then(response => setCalls({
             nodes: response.nodes,
             totalCount: response.totalCount,
             hasNextPage: response.hasNextPage
         }))
     }, []);
 
+    console.log(calls.nodes)
+
     return (
-        <div className="calls">
-            <div className="call">
-                asdfafsdsfd
+        <div className="container-fluid d-flex justify-content-center">
+            <div className="calls col-10">
+                {calls.nodes.map((call) => 
+                    <div className="call" key={call.id}>
+                        <p>From: {call.from} </p>
+                        <p>To: {call.to}</p>
+                        <p>Via :{call.via}</p>
+                    </div>
+                )}
                 {calls.totalCount}
             </div>
         </div>
--- aircall-app/src/util/api-con.js
+++ aircall-app/src/util/api-con.js
@@ -20,16 +20,32 @@ export async function authUser() {
     }
 }
 
-export async function getCalls(access_token) {
-    try{
-        let response = await axios.get(url + '/calls?offset=5&limit=10' , { 'headers': { 'Authorization': "Bearer "+ access_token }});
-        
+export async function refresh_token(user) {
+    try {
+        const response = await axios.post(url + '/auth/refresh-token-v2', { 'headers': { 'Authorization': "Bearer " + user.refresh_token } })
+
+        console.log(response)
+        return response.status === 200 ? {
+            access_token: response.data.access_token,
+            refresh_token: response.data.refresh_token,
+            user: response.data.user
+        } : null;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+export async function getCalls(user) {
+    try {
+        let response = await axios.get(url + '/calls?offset=1&limit=10', { 'headers': { 'Authorization': "Bearer " + user.access_token } });
+
         return {
             nodes: response.data.nodes,
             totalCount: response.data.totalCount,
             hasNextPage: response.data.hasNextPage
         }
-    } catch(error) {
+    } catch (error) {
         console.log(error);
         return null;
     }
