7e4383d756e2c2eb0a2e7a64ded3d9ed115859fa
Thiago Túlio Brandão Silva <47676524+ttbs1@users.noreply.github.com>
2021-12-04 00:13:26 -0300

    call detail display info

--- aircall-app/src/pages/call.jsx
+++ aircall-app/src/pages/call.jsx
@@ -1,11 +1,38 @@
 
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { getCall } from "../util/api-con";
 
-export function Call(props) {
+export function Call() {
     let { id } = useParams();
-    console.log(props);
+    const [call, setCall] = useState({});
+
+    useEffect(() => {
+        getCall(id).then(response => setCall({
+            duration: response.duration,
+            is_archived: response.is_archived,
+            from: response.from,
+            to: response.to,
+            direction: response.direction,
+            call_type: response.call_type,
+            via: response.via,
+            created_at: response.created_at,
+            notes: response.notes
+        }))
+    }, []);
 
     return (
-        <div>{id}</div>
+        <div className="container">
+            <p></p>
+            <p>{id}</p>
+            <p>Duration: {call.duration}</p>
+            <p>Is archived: {call.is_archived}</p>
+            <p>From: {call.from}</p>
+            <p>To: {call.to}</p>
+            <p>Direction: {call.direction}</p>
+            <p>Call Type: {call.call_type}</p>
+            <p>Via: {call.via}</p>
+            <p>Created at: {call.created_at}</p>
+        </div>
     )
 }
--- aircall-app/src/util/api-con.js
+++ aircall-app/src/util/api-con.js
@@ -80,4 +80,26 @@ export async function getCalls(index, offset) {
         console.log(error);
         return null;
     }
+}
+
+export async function getCall(id) {
+    try {
+        let user = JSON.parse(sessionStorage.user_data);
+        let response = await axios.get(url + `/calls/${id}`, { headers: { 'Authorization': "Bearer " + user.access_token } });
+
+        return {
+            duration: response.data.duration,
+            is_archived: response.data.is_archived,
+            from: response.data.from,
+            to: response.data.to,
+            direction: response.data.direction,
+            call_type: response.data.call_type,
+            via: response.data.via,
+            created_at: response.data.created_at,
+            notes: response.data.notes
+        } ;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
 };
